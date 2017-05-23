858b1ed6a3f59c96eb8e6cdc4db8c061616bf005
BrillitVivas <bri.vivas20@gmail.com>
2017-05-23 08:04:07 -0500

    1subida
    aplicacion9

---
+++ js/acciones.js
@@ -0,0 +1,73 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", onDeviceReady, false );
+	
+});
+
+function onDeviceReady(){
+	
+	$('#posicion').on('click', function (){
+		getPosition();
+		});
+		
+		$('#watch').on('click',function(){
+			watchPosition();
+			});
+			
+}
+
+function getPosition(){
+	
+	var options={
+		enableHighAccuary : true,
+		maximumAge: 3600000
+	}
+	
+	var WatchID= navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	
+	function onSuccess(position){
+		
+		alert('Latitude:'    + position.coords.latitude   +'\n'+
+		'Longitude:'       + position.coords.longitude   +'\n'+
+		'Altitude:'      + position.coords.altitude     +'\n'+ 
+        'Accuracy:'     + position.coords.accuracy         +'\n'+
+		'Altitude Accuracy:' + position.coords.altitudeAccuracy +'\n'+
+		'Heading:'        + position.coords.heading    +'\n'+
+		'Speed:'      + position.coords.speed      +'\n'+
+		'Timestamp:'      + position.timestamp     +'\n');
+	};
+		
+		function onError(Error){
+			alert('code:'   + error.code  +'\n'+'message:'+error.message+'\n');
+		}
+}
+
+function WatchPosition(){
+	
+	var options={
+		maximumAge : 3600000,
+		timeout : 3000,
+		enableHighAccuracy : true,
+	}
+	
+	var WatchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+	
+	function onSuccess(position){
+		
+		$('#latitud').html(position.coord.latitude);
+		$('#longitude').html(position.coord.longitude);
+		$('#altitud').html(position.coord.altitud);
+		$('#accuracy').html(position.coord.accuracy);
+		$('#aaccuracy').html(position.coord.aaccuracy);
+		$('#headingg').html(position.coord.headingg);
+		$('#speed').html(position.coord.speed);
+		$('#timestamp').html(position.coord.timestamp);
+		};
+		
+		function onError(error){
+			alert('code:'   +error.code  +'\n'+'message:' +error.message +'\n');
+		}
+		
+}
+						
